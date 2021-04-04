bc903ed3014ea32f1c9c47202e95bcdd671fef3b
Md Abul Hashim Rafsan Khan <hashim.khan@northsouth.edu>
2021-04-04 17:31:27 +0600

    Created delivery boy model controller and added delivery boy register table

--- resources/views/BackEnd/include/sidebar.blade.php
+++ resources/views/BackEnd/include/sidebar.blade.php
@@ -24,7 +24,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item has-treeview menu-open">
-                    <a href="#" class="nav-link active">
+                    <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Category
@@ -46,6 +46,31 @@
                         </li>
                     </ul>
                 </li>
+
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Delivery Boy
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{route('show_deliveryBoy_add_table')}}" class="nav-link active">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Add Delivery Boy</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{--{{route('manage_cate')}}--}}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Manage Delivery Boy</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
-/*Category starts here*/
 
+/*Category starts here*/
 Route::get('/category/add','App\Http\Controllers\categoryController@index')->name('show_cate_table');
 Route::post('/category/save','App\Http\Controllers\categoryController@save')->name('cate_save');
 Route::get('/category/manage','App\Http\Controllers\categoryController@manage')->name('manage_cate');
@@ -28,5 +28,10 @@ Route::get('/category/active/{category_id}','App\Http\Controllers\categoryContro
 Route::get('/category/inactive/{category_id}','App\Http\Controllers\categoryController@inactive')->name('inactive_cate');
 Route::get('/category/delete/{category_id}','App\Http\Controllers\categoryController@delete')->name('cate_delete');
 Route::post('/category/update','App\Http\Controllers\categoryController@update')->name('cate_update');
+
 /*Category ends here*/
+/*Delivery Boy Starts here*/
+Route::get('/delivery/boy/add','App\Http\Controllers\deliveryBoyController@index')->name('show_deliveryBoy_add_table');
+Route::post('/delivery/boy/save','App\Http\Controllers\deliveryBoyController@save_boy')->name('delivery_save');
+/*Delivery Boy Ends here*/
 
