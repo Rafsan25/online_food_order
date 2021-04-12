b44183d85b93911344223ecbe301203c36cf07a9
Md Abul Hashim Rafsan Khan <hashim.khan@northsouth.edu>
2021-04-13 01:04:22 +0600

    Modified frontend template.Created files and controllers for frontend

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/','App\Http\Controllers\FrontEndController@index');
 Auth::routes();
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
